ea5bd7fa09bb542656d5a748e3666b30e6e50bfe
Tomas Cejas <Tomas.rcv@gmail.com>
2022-10-26 00:15:29 -0300

    comiteo para ver que onda

--- app.js
+++ app.js
@@ -5,5 +5,13 @@ app.use(express.static('public'))
 app.listen(3000)
 
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve('./views/home.html'))
+  res.sendFile(path.resolve(__dirname,"./views/home.html"))
+})
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.resolve(__dirname,"./views/login.html"))
+})
+
+app.get("/recuperacion", (req, res) => {
+  res.sendFile(path.resolve(__dirname,"./views/r-contrasena.html"))
 })
