91dfe0af13d48c69ee703aaba9ff312985db91ae
ricardoarielmartinez <ricardoarielmartinez@gmail.com>
2023-01-30 17:57:41 -0300

    Se agrega login desde BD

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -7,6 +7,11 @@ const usersFilePath = path.join(__dirname, "../database/users.json");
 const users = JSON.parse(fs.readFileSync(usersFilePath, "utf-8"));
 const { validationResult } = require("express-validator");
 
+/*DB*/
+const db = require("../database/models");
+const sequelize = db.sequelize;
+/*DB*/
+
 const userController = {
 	contrasena: (req, res) => {
 		res.render("r-contrasena");
@@ -16,72 +21,89 @@ const userController = {
 	},
 	userStore: (req, res) => {
 		let errores = validationResult(req);
-		if (typeof users.find((user) => user.email == req.body.email) != undefined) {
-			errores.errors.push({
-				value: "",
-				msg: "El usuario ya existe.",
-				param: "emailRepetido",
-				location: "body",
-			});
-		}
-		if (errores.isEmpty()) {
-			let newUser = {
-				id: users[users.length - 1].id + 1,
-				nombre: req.body.nombre,
-				apellido: req.body.apellido,
+
+		db.Users.findAll({
+			where: {
 				email: req.body.email,
-				password: bcrypt.hashSync(req.body.password, salt),
-				categoria: 2,
-				imagen: req.file ? req.file.filename : "default-image.jpg",
-			};
-			users.push(newUser);
-			fs.writeFileSync(usersFilePath, JSON.stringify(users, null, " "));
-			res.redirect("/login");
-		} else {
-			res.render("register", { errores: errores.mapped() });
-		}
+			},
+		}).then((dbUsers) => {
+			if (dbUsers.length > 0) {
+				errores.errors.push({
+					value: "",
+					msg: "El usuario ya existe.",
+					param: "emailRepetido",
+					location: "body",
+				});
+			}
+			if (errores.isEmpty()) {
+				let newUser = {
+					name: req.body.nombre,
+					apellido: req.body.apellido,
+					email: req.body.email,
+					password: bcrypt.hashSync(req.body.password, salt),
+					id_level: 2,
+					imagen: req.file ? req.file.filename : "default-image.jpg",
+				};
+				db.Users.create(newUser)
+					.then(res.redirect("/login"))
+					.catch((error) => res.send(error));
+			} else {
+				res.render("register", { errores: errores.mapped() });
+			}
+		});
 	},
+
+	loginUser: (req, res) => {
+		let errores = validationResult(req);
+		db.Users.findAll({
+			where: {
+				email: req.body.email.trim(),
+			},
+		})
+		.then((dbUser)=>{
+			if (dbUser.length <= 0) {
+				errores.errors.push({
+					value: "",
+					msg: "El usuario no existe.",
+					param: "email",
+					location: "body",
+				});
+			} else {
+				if (!bcrypt.compareSync(req.body.password, dbUser[0].dataValues.password))
+					errores.errors.push({
+						value: "",
+						msg: "La contraseña es incorrecta.",
+						param: "password",
+						location: "body",
+					});
+			}
+			
+			if (errores.isEmpty()) {
+				dbUser[0].password = "";
+				req.session.userLogged = {...dbUser[0].dataValues};				
+				if (req.body.checkbox) {
+					res.cookie("userEmail", dbUser[0].email, {
+						maxAge: 1000 * 60 * 60 * 24,
+					});
+				}
+				return res.redirect("/")
+			}else {
+				return res.render("login", { errores: errores.mapped() });
+			}
+
+
+		})
+	},
+
 	login: (req, res) => {
 		res.render("login");
 	},
-	loginUser: (req, res) => {
-		let errores = validationResult(req);
-		let usuarioEncontrado = users.find(
-			(user) => user.email === req.body.email.trim()
-		);
-		let useraux = {...usuarioEncontrado}
-		if (typeof useraux === undefined && req.body.email !== "") {
-			errores.errors.push({
-				value: "",
-				msg: "El usuario no existe.",
-				param: "emailInexistente",
-				location: "body",
-			});
-		} else {
-			if (!bcrypt.compareSync(req.body.password, useraux.password))
-				errores.errors.push({
-					value: "",
-					msg: "La contraseña es incorrecta.",
-					param: "contrasenaIncorrecta",
-					location: "body",
-				});
-		}
-		if (errores.isEmpty()) {
-			delete useraux.password;
-			req.session.userLogged = useraux;
-			if(req.body.checkbox){
-				res.cookie('userEmail', useraux.email,{maxAge: 1000 * 60 * 60 * 24})
-			  }
-			res.redirect("/");
-		} else {
-			res.render("login", { errores: errores.mapped() });
-		}
-	},
-	logout:(req,res)=>{
+
+	logout: (req, res) => {
 		req.session.destroy();
-		res.cookie('userEmail',null,{maxAge: -1});
-		return res.redirect("/")
-	}
+		res.cookie("userEmail", null, { maxAge: -1 });
+		return res.redirect("/");
+	},
 };
 
 module.exports = userController;
