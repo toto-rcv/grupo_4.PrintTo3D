8c807f20aa1aa1887b04b26ef20d159356e3b020
24Viloria <devluisviloria@gmail.com>
2023-02-21 22:32:46 -0300

    validaciones de creacion y modificacion de producto, y validaciones de login de usuario

--- src/routes/productRoute.js
+++ src/routes/productRoute.js
@@ -5,15 +5,60 @@ const authMiddleware = require("../middlewares/authMiddleware")
 const multer = require("multer");
 const path = require("path");
 const { body } = require("express-validator");
-
+let fileLocal;
 const validaciones = [
-    body('name').notEmpty().withMessage('Tiene que ingresar un Nombre'),
-	body('name').isLength({ min: 5 }).withMessage('El Nombre debe tener mas de 5 caracteres'),
-	body('short_description').notEmpty().withMessage('Tiene que ingresar una Descripción'),
-	body('short_description').isLength({ min: 20 }).withMessage('Tiene que ingresar una Descripción de mas de 20 caracteres'),
-	body('long_description').notEmpty().withMessage('Tiene que ingresar una Descripción'),
-	body('long_description').isLength({ min: 20 }).withMessage('Tiene que ingresar una Descripción de mas de 20 caracteres'),
+    body('name')
+		.notEmpty().withMessage('Tiene que ingresar un nombre')
+		.isLength({ min: 5 }).withMessage('El nombre debe tener mas de 5 caracteres'),
+	body('short_description')
+		.notEmpty().withMessage('Tiene que ingresar una descripción')
+		.isLength({ min: 20 }).withMessage('Tiene que ingresar una descripción de mas de 20 caracteres'),
+	body('long_description')
+		.notEmpty().withMessage('Tiene que ingresar una descripción')
+		.isLength({ min: 50 }).withMessage('Tiene que ingresar una descripción de más de 20 caracteres'),
 	body('price').notEmpty().withMessage('Tiene que ingresar un precio'),
+	body('imageFormat').custom(async (value, {req}) => {
+		let file = fileLocal;
+		let acceptedExtensions = ['.jpg', '.png', '.gif'];
+	
+		if (!file) {
+			throw new Error('Tienes que subir una imagen');
+		} else {
+			let fileExtension = path.extname(file.originalname);
+			if (!acceptedExtensions.includes(fileExtension)) {
+				throw new Error(`Las extensiones de archivo permitidas son ${acceptedExtensions.join(', ')}`);
+			}
+		}
+	
+		return true;
+		})
+]
+const validacionesEdit = [
+    body('nombreProducto')
+		.notEmpty().withMessage('Tiene que ingresar un Nombre')
+		.isLength({ min: 5 }).withMessage('El Nombre debe tener mas de 5 caracteres'),
+	body('descripcion')
+		.notEmpty().withMessage('Tiene que ingresar una Descripción')
+		.isLength({ min: 20 }).withMessage('Tiene que ingresar una Descripción de mas de 20 caracteres'),
+	body('descripcionAmpliada')
+		.notEmpty().withMessage('Tiene que ingresar una Descripción')
+		.isLength({ min: 50 }).withMessage('Tiene que ingresar una Descripción de mas de 50 caracteres'),
+	body('precioProducto').notEmpty().withMessage('Tiene que ingresar un precio'),
+	body('imageFormat').custom(async (value, {req}) => {
+		let file = fileLocal;
+		let acceptedExtensions = ['.jpg', '.png', '.gif'];
+	
+		if (!file) {
+			throw new Error('Tienes que subir una imagen');
+		} else {
+			let fileExtension = path.extname(file.originalname);
+			if (!acceptedExtensions.includes(fileExtension)) {
+				throw new Error(`Las extensiones de archivo permitidas son ${acceptedExtensions.join(', ')}`);
+			}
+		}
+	
+		return true;
+		})
 ]
 const storage = multer.diskStorage({
 	destination: function (req, file, cb) {
@@ -24,7 +69,14 @@ const storage = multer.diskStorage({
 	},
 });
 
-const upload = multer ({storage})
+const upload = multer ({storage,fileFilter: (req, file, cb) => {
+	fileLocal = file
+	  if (file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/jpeg") {
+		  cb(null, true);
+	  } else {
+	  cb(null, false);
+	  }
+  }})
 
 router.get("/busqueda", productController.busqueda);
 
@@ -41,7 +93,7 @@ router.post("/products/create", upload.single('image'), validaciones, productCon
 
 /*** EDIT ONE PRODUCT ***/
 router.get('/products/:id/edit',  productController.edit);
-router.put('/products/:id', upload.single("image"), productController.update);
+router.put('/products/:id/edit', upload.single("image"), validacionesEdit, productController.update);
 router.delete('/products/:id', productController.delete);
 
 /*** GET ONE PRODUCT ***/
