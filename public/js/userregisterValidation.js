3fe7f6f9b8b608dc02827dcbf27cb0a5057ed0a9
Sofia Chaves <sofiamagdalenachaves@gmail.com>
2023-02-22 00:15:56 -0300

    validaciones front.

--- public/js/userregisterValidation.js
+++ public/js/userregisterValidation.js
@@ -8,6 +8,10 @@ let emailError = document.getElementById("emailError")
 let passwordError = document.getElementById("passwordError")
 let confirmPasswordError = document.getElementById("confirmPasswordError")
 
+//function cambio(e){
+  //  console.log("Messi")
+    //console.log(e)
+//
 
 
 form.addEventListener("submit", (evento) => {
@@ -17,15 +21,23 @@ form.addEventListener("submit", (evento) => {
     let email = document.getElementById("email");
     let password = document.getElementById("password");
     let confirmPassword = document.getElementById("confirmPassword");
-   // let image = document.getElementById("image");
- 
+    let image = document.getElementById("image");
+    console.log(image)
  
     nombreError.innerText= nombre.value ? (nombre.value.length <= 2 ? "El nombre debe ser mayor a dos caracteres" : "") : "El Nombre no puede estar vacio"
     apellidoError.innerText= !apellido.value ? "El Apellido no puede estar vacio" : (apellido.value.length <= 2 ? "El apellido debe ser mayor a dos caracteres" : "") 
     emailError.innerText= !email.value ?  "El email no puede estar vacio" : ""
-    passwordError.innerText= !password.value ? "Debe ingresar una contraseña" : (password.value !== confirmPassword.value ? "Las contraseñas no coinciden" : "")
+   if(!!password.value){
+    if(!password.value.matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/, 'i')){
+        passwordError.innerText = "La contraseña debe tener al menos una mayúscula, una minúscula y un número, y debe tener una longitud mínima de 8 caracteres"
+    }else if(password.value !== confirmPassword.value){
+        passwordError.innerText = "Las contraseñas no coinciden"
+    }
+   }else {
+    passwordError.innerText = "Debe ingresar una contraseña"
+   }
     //imageError.innerText= ""
-    //.matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/, 'i')
+    
    
     if(!!nombreError.innerText || !!apellidoError.innerText || !!emailError.innerText || !!passwordError.innerText){
         evento.preventDefault();
